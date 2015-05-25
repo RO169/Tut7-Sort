b589c50e9412d013d5a63082d7f48ed5c258f889
RO169 <rowen.rungiah@gmail.com>
2015-05-25 15:11:02 +0200

    created a print template function

--- Tutorial 7/main_Tut7.cpp
+++ Tutorial 7/main_Tut7.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 
@@ -7,6 +8,22 @@ T sort(T myArray[], int nElem);
 
 int main()
 {
+	string strArray[] = { "a", "z", "d", "f", "R" };
+	float fltArray[] = { 5.37, 7.65, 1.78, 6.23, 5.56 };
+	int inArray[] = { 6, 5, 2, 8, 1 };
+
+	int elems = 5;
+	string sortStrArray[] = sort(strArray[5], elems);
+	float sortFltArray[] = sort(fltArray[5], elems);
+	int sortinArray[] = sort(inArray[5], elems);
+
+	cout << "The string array is:\n" << print(strArray[5], 5) << endl;
+	cout << "The sorted string array is:\n" << print(sortStrArray[5], 5) << endl;
+	cout << "\n\nThe float array is:\n" << print(fltArray[5], 5) << endl;
+	cout << "The sorted float array is:\n" << print(sortFltArray[5], 5) << endl;
+	cout << "\n\nThe int array is:\n" << print(inArray[5], 5) << endl;
+	cout << "The sorted int array is:\n" << print(sortinArray[5], 5) << "\n\n\n";
+
 
 	system("Pause");
 	return 0;
@@ -24,4 +41,13 @@ T sort(T myArray[], int nElem)
 		}
 	}
    		return myArray[];
+}
+
+template <typename T2>
+void print(T2 anyArray[], int nElms)
+{
+	for (int i = 0; i < nElms; i++)
+	{
+		cout << anyArray[i] << "\t";
+	}
 }
