50e385566dd98ff0daeb8d0a4d0fb0dbf64ebe6e
RO169 <rowen.rungiah@gmail.com>
2015-05-25 17:32:12 +0200

    debugged passing array, and sort function

--- Tutorial 7/main_Tut7.cpp
+++ Tutorial 7/main_Tut7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 template <typename T>
-T sort(T myArray[], int nElem);
+void sort(T myArray[], int nElem);
 template <typename T2>
 void print(T2 anyArray[], int nElms);
 
@@ -17,15 +17,14 @@ int main()
 
 	int elems = 5;
 	
-	//cout << "The string array is:\n" << print(strArray[5], elems) << endl;
-	//cout << "The sorted string array is:\n" << print(sort(strArray[5], elems), elems) << endl;
-	//cout << "\n\nThe float array is:\n" << print(fltArray[5], elems) << endl;
-	//cout << "The sorted float array is:\n" << print(sort(fltArray[5], elems), elems) << endl;
-	//cout << "\n\nThe int array is:\n" << print(inArray[5], elems) << endl;
-	//cout << "The sorted int array is:\n" << print(sort(inArray[5], elems), elems) << "\n\n\n";
+	cout << "The string array is:\n" << endl;
+	print(strArray, elems);
+	cout << "\n\nThe float array is:\n" << endl;
+	print(fltArray, elems);
+	cout << "\n\nThe int array is:\n" << endl;
+	print(inArray, elems);
 
-	
-	//print(inArray[5], 5);
+	cout << "\n\n";
 
 	system("Pause");
 	return 0;
@@ -34,12 +33,17 @@ int main()
 template <typename T>
 void sort(T myArray[], int nElem)
 {
-		for (int i = 0; i < nElem, i++)
+	T temp;
+	for (int i = 0; i < nElem; i++)
 	{
 		for (int j = i; j < nElem; j++)
 		{
 			if (myArray[j] < myArray[i])
+			{
+				temp = myArray[i];
 				myArray[i] = myArray[j];
+				myArray[j] = temp;
+			}
 		}
 	}
 }
@@ -47,6 +51,10 @@ void sort(T myArray[], int nElem)
 template <typename T2>
 void print(T2 anyArray[], int nElms)
 {
+	for (int i = 0; i < nElms; i++)
+		cout << anyArray[i] << "\t";
+	cout << "\n\nThe sorted Array is:\n\n";
+	sort(anyArray, nElms);
 	for (int i = 0; i < nElms; i++)
 		cout << anyArray[i] << "\t";
 }
